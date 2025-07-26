9b331f432e98299b516bc823476b10290554efed
Attila <hunkrekker@gmail.com>
2025-07-26 15:08:05 +0200

    Implemented Camera interface

---
+++ ELink/Interfaces/Hardware/ICamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELink.Interfaces.Hardware
+{
+    public interface ICamera
+    {
+        public void SetExposureLegth(float time);
+        public void SetGain(int gain);
+        public int TakeFrame();
+    }
+}
