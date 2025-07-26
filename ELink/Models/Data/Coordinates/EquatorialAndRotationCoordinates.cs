9c68128ac644be725afb1b0163aeb17c025c6506
Attila <hunkrekker@gmail.com>
2025-07-26 14:47:43 +0200

    Implemented coordinate DTOs

---
+++ ELink/Models/Data/Coordinates/EquatorialAndRotationCoordinates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELink.Models.Data.Coordinates
+{
+    public struct EquatorialAndRotationCoordinates
+    {
+        public double Ra { get; set; }
+        public double Dec { get; set; }
+        public double R { get; set; }
+        public EquatorialAndRotationCoordinates()
+        {
+        }
+
+        public static implicit operator EquatorialCoordiantes(EquatorialAndRotationCoordinates EQR)
+        {
+            return new EquatorialAndRotationCoordinates { Ra = EQR.Ra, Dec = EQR.Dec};
+        }
+    }
+}
---
+++ ELink/Models/Data/Coordinates/EquatorialCoordiantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELink.Models.Data.Coordinates
+{
+    public struct EquatorialCoordiantes
+    {
+        public double Ra { get; set; }
+        public double Dec { get; set; }
+
+        public EquatorialCoordiantes()
+        {
+
+        }
+
+        public static implicit operator EquatorialAndRotationCoordinates(EquatorialCoordiantes EQ)
+        {
+            return new EquatorialAndRotationCoordinates { Ra = EQ.Ra, Dec = EQ.Dec, R = 0 };
+        }
+    }
+}
