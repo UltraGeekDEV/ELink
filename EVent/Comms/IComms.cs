b0f8ee1f962779ecba7339b4a9649c30bf2dff69
Attila <hunkrekker@gmail.com>
2025-07-31 12:45:41 +0200

    Added base interfaces

---
+++ EVent/Comms/IComms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVent.Comms
+{
+    public interface IComms<T>
+    {
+        public void Send(T data);
+    }
+}
---
+++ EVent/Comms/IConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVent.Comms
+{
+    public interface IConnection
+    {
+        public void SendData(byte[] data);
+        public void ExpectData<T>(string eventID, IComms<T> handler);
+    }
+}
